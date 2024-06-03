9bb3a1d9970dbf626ad39a7b35cb11c7011c6a1c
cvega <carlosjuniorrvega@hotmail.com>
2024-06-03 16:05:17 -0500

    feat(#):format. CV

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Switch from 'react-switch';
+import Switch from "react-switch";
 import "./header.css";
 
 const Header = () => {
@@ -12,32 +12,38 @@ const Header = () => {
             <li className="nav__menu--items">
               <a href="#home">Home</a>
             </li>
+
             <li className="nav__menu--items">
               <a href="#services">Services</a>
             </li>
+
             <li className="nav__menu--items">
               <a href="#skills">Skills</a>
             </li>
+
             <li className="nav__menu--items">
               <a href="#portfolio">Portfolio</a>
             </li>
+
             <li className="nav__menu--items">
               <a href="#contact">Contact</a>
             </li>
-            <li className='buttons'>
-						<label htmlFor='' className='switch'>
-							<Switch
-								height={24}
-								width={48}
-								onColor='#4D4D4D'
-								offColor='#ccc'
-                checked = {true}
-							/>
-						</label>
-						<a href='' className='button'>
-							Hire Me
-						</a>
-					</li>
+
+            <li className="buttons">
+              <label htmlFor="" className="switch">
+                <Switch
+                  height={24}
+                  width={48}
+                  onColor="#4D4D4D"
+                  offColor="#ccc"
+                  checked={true}
+                />
+              </label>
+
+              <a href="" className="button">
+                Hire Me
+              </a>
+            </li>
           </ul>
         </div>
       </div>

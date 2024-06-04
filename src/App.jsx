124d3e57fbae27e37f1c290a117cd4fb9bd874c8
cvega <carlosjuniorrvega@hotmail.com>
2024-06-03 19:46:00 -0500

    feat(#context): wrapping in ThemeProvider. CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/header/Header'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
     <>
-     <div className="app">
-     <Header/>
-     <Router>
-      <Routes>
-        <Route></Route>
-      </Routes>
-     </Router>
-     </div>
+        <ThemeProvider>
+          <Header />
+          <Router>
+            <Routes>
+              <Route></Route>
+            </Routes>
+          </Router>
+        </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
